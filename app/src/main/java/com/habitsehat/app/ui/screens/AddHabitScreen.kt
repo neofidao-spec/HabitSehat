48fb8609397dc430d668a0f3978ed9a841d863bf
Fani Hernanda <nandaori04@gmail.com>
2026-07-14 19:33:24 +0800

    Fix: missing sp import in AddHabitScreen

--- app/src/main/java/com/habitsehat/app/ui/screens/AddHabitScreen.kt
+++ app/src/main/java/com/habitsehat/app/ui/screens/AddHabitScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.habitsehat.app.data.model.Habit
 
 @OptIn(ExperimentalMaterial3Api::class)
